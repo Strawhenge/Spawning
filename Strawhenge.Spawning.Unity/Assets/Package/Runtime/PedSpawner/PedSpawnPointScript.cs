effd8cf836c984c24ce248b519d30617a00d503b
Matt Bristow <strawhengestudios@gmail.com>
2023-01-24 22:51:36 +0000

    initial project import

---
+++ Strawhenge.Spawning.Unity/Assets/Package/Runtime/PedSpawner/PedSpawnPointScript.cs
@@ -0,0 +1,69 @@
+﻿using Strawhenge.Common.Unity;
+using System.Collections;
+using UnityEngine;
+using static UnityEngine.Random;
+
+namespace Strawhenge.Spawning.Unity.PedSpawner
+{
+    public class PedSpawnPointScript : MonoBehaviour
+    {
+        [SerializeField] GameObject[] _spawnablePeds;
+        [SerializeField] EventScriptableObject[] _onSpawnEvents;
+        [SerializeField, Tooltip("In seconds")] float _cooldown;
+        [SerializeField, Tooltip("Allows spawning while in camera view")] bool _isEntrance;
+
+        bool _isAwaitingCooldown;
+
+        void OnEnable()
+        {
+            if (_spawnablePeds.Length == 0)
+            {
+                Debug.LogError("Spawn point has no spawnable peds.", this);
+                enabled = false;
+            }
+        }
+
+        internal bool CanSpawn(ISpawnChecker spawnChecker)
+        {
+            if (_isAwaitingCooldown)
+                return false;
+
+            return _isEntrance
+                ? spawnChecker.CanSpawnInEntrance(transform.position)
+                : spawnChecker.CanSpawn(transform.position);
+        }
+
+        internal GameObject SpawnOne()
+        {
+            var ped = Instantiate(
+                _spawnablePeds[Range(0, _spawnablePeds.Length)],
+                transform.position,
+                transform.rotation);
+
+            StartCoroutine(RunEvents());
+
+            IEnumerator RunEvents()
+            {
+                yield return new WaitForEndOfFrame();
+
+                foreach (var @event in _onSpawnEvents)
+                    @event.Invoke(ped);
+            }
+
+            StartCooldown();
+            return ped;
+        }
+
+        void StartCooldown()
+        {
+            _isAwaitingCooldown = true;
+
+            StartCoroutine(Cooldown());
+            IEnumerator Cooldown()
+            {
+                yield return new WaitForSeconds(_cooldown);
+                _isAwaitingCooldown = false;
+            }
+        }
+    }
+}
---
+++ Strawhenge.Spawning.Unity/Assets/Package/Runtime/SpawnChecker.cs
@@ -0,0 +1,51 @@
+﻿using Strawhenge.Common.Unity;
+using UnityEngine;
+
+namespace Strawhenge.Spawning.Unity
+{
+    public class SpawnChecker : ISpawnChecker
+    {
+        internal Camera Camera { private get; set; }
+
+        internal GameObject Player { private get; set; }
+
+        internal float DespawnDistance { private get; set; }
+
+        internal float SpawnDistance { private get; set; }
+
+        internal float SpawnDistanceWhenVisible { private get; set; }
+
+        internal float EntranceSpawnDistance { private get; set; }
+
+        public bool CanSpawn(Vector3 position)
+        {
+            var distance = GetDistanceTo(position);
+
+            return CanSee(position)
+                ? distance >= SpawnDistanceWhenVisible
+                : distance >= SpawnDistance;
+        }
+
+        public bool CanSpawnInEntrance(Vector3 position) => GetDistanceTo(position) >= EntranceSpawnDistance;
+
+        public bool CanDespawn(GameObject gameObject) =>
+            !CanSee(gameObject.transform.position) &&
+            GetDistanceTo(gameObject.transform.position) >= DespawnDistance;
+
+        public float GetDistanceTo(Vector3 position)
+        {
+            if (Player == null)
+                return 0;
+
+            return Vector3.Distance(position, Player.transform.position);
+        }
+
+        bool CanSee(Vector3 position)
+        {
+            if (Camera == null)
+                return false;
+
+            return Camera.IsVisible(position);
+        }
+    }
+}
