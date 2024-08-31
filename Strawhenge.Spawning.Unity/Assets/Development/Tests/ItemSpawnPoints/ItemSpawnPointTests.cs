39c4ad816bf87b1b2e70da99ed9bb89f5fd470d7
Matt Bristow <strawhengestudios@gmail.com>
2024-08-31 15:38:53 +0100

    added initial spawnpoints test

---
+++ Strawhenge.Spawning.Unity/Assets/Development/Tests/ItemSpawnPoints/ItemSpawnPointTests.cs
@@ -0,0 +1,72 @@
+using NUnit.Framework;
+using Strawhenge.Spawning.Unity;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class ItemSpawnPointTests
+{
+    const string Scene = "ItemSpawnPointTests";
+    ItemSpawnPointTestsScript _context;
+
+    [UnitySetUp]
+    public IEnumerator LoadScene()
+    {
+        yield return SceneManager.LoadSceneAsync(Scene);
+        _context = Object.FindObjectOfType<ItemSpawnPointTestsScript>();
+
+        if (_context == null || _context.IsInvalid())
+            Assert.Fail("Test context is invalid.");
+    }
+   
+    [UnityTest]
+    public IEnumerator Items_should_spawn_when_player_is_near_spawn_points()
+    {
+        VerifyNoSpawns();
+        yield return new WaitForFixedUpdate();
+
+        yield return SpawnPointShouldSpawnWhenPlayerMovesNear();
+        yield return SpawnPointsShouldNotRespawnWhenPlayerReturns();
+    }
+
+    IEnumerator SpawnPointShouldSpawnWhenPlayerMovesNear()
+    {
+        int expectedNumberOfSpawns = 0;
+
+        foreach (var spawnPoint in _context.SpawnPoints)
+        {
+            MovePlayerTo(spawnPoint);
+            yield return new WaitForFixedUpdate();
+            
+            expectedNumberOfSpawns++;
+            VerifyNumberOfSpawns(expectedNumberOfSpawns);
+        }
+    }
+    
+    IEnumerator SpawnPointsShouldNotRespawnWhenPlayerReturns()
+    {
+        var numberOfSpawns = GetNumberOfSpawns();
+        
+        foreach (var spawnPoint in _context.SpawnPoints)
+        {
+            MovePlayerTo(spawnPoint);
+            yield return new WaitForFixedUpdate();
+            
+            VerifyNumberOfSpawns(numberOfSpawns);
+        }
+    }
+
+    void VerifyNumberOfSpawns(int numberOfSpawns)
+    {
+        var actualSpawns = GetNumberOfSpawns();
+        TestContext.WriteLine($"Spawns: {actualSpawns}");
+        Assert.AreEqual(numberOfSpawns, actualSpawns);
+    }
+
+    void MovePlayerTo(ItemSpawnPointScript spawnPoint) => _context.MovePlayerTo(spawnPoint);
+
+    static void VerifyNoSpawns() => Assert.Zero(GetNumberOfSpawns());
+
+    static int GetNumberOfSpawns() => Object.FindObjectsOfType<ItemSpawnScript>().Length;
+}
---
+++ Strawhenge.Spawning.Unity/Assets/Development/Tests/ItemSpawnPoints/ItemSpawnPointTestsScript.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using Strawhenge.Spawning.Unity;
+using UnityEngine;
+
+public class ItemSpawnPointTestsScript : MonoBehaviour
+{
+    [SerializeField] GameObject _player;
+
+    public ItemSpawnPointScript[] SpawnPoints { get; private set; }
+
+    void Awake()
+    {
+        SpawnPoints = FindObjectsOfType<ItemSpawnPointScript>();
+    }
+
+    public bool IsInvalid()
+    {
+        var valid = true;
+
+        if (_player == null)
+        {
+            TestContext.WriteLine("Player not set.");
+            valid = false;
+        }
+
+        TestContext.WriteLine($"{SpawnPoints.Length} spawn points in scene.");
+        if (SpawnPoints.Length == 0)
+            valid = false;
+
+        return !valid;
+    }
+
+    public void MovePlayerTo(ItemSpawnPointScript spawnPoint)
+    {
+        TestContext.WriteLine($"Moving player to spawn point {spawnPoint.name}.");
+        _player.transform.position = spawnPoint.transform.position;
+    }
+}
