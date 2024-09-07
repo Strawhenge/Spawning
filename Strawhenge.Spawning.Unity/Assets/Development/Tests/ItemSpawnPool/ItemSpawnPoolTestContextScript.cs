a866567b64e381650dfa08c0506792cf74399e68
Matt Bristow <strawhengestudios@gmail.com>
2024-09-07 16:16:12 +0100

    refactor and tidy up demo scene

--- Strawhenge.Spawning.Unity/Assets/Development/Tests/ItemSpawnPool/ItemSpawnPoolTestContextScript.cs
+++ Strawhenge.Spawning.Unity/Assets/Development/Tests/ItemSpawnPool/ItemSpawnPoolTestContextScript.cs
@@ -5,28 +5,12 @@ namespace Strawhenge.Spawning.Unity.Tests
     public class ItemSpawnPoolTestContextScript : BaseTestContextScript
     {
         [SerializeField] ItemSpawnScript _prefab;
-        [SerializeField, Min(1)] int _quantity = 3;
-
-        ItemSpawnPool _pool;
 
         public ItemSpawnScript Prefab => _prefab;
 
         public override bool IsInvalid()
         {
             return _prefab == null;
-        }
-
-        [ContextMenu(nameof(Spawn))]
-        public void Spawn()
-        {
-            _pool
-                .TryGet()
-                .Do(spawn => spawn.transform.SetPositionAndRotation(transform.position, transform.rotation));
-        }
-
-        void Awake()
-        {
-            _pool = new ItemSpawnPool(_prefab, _quantity);
         }
     }
 }
